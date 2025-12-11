aff81d39139d3d4c16fbf17f3eacff489be21e7a
Roman <topmanov@gmail.com>
2025-12-11 14:31:55 +0200

    Добавьте файлы проекта.

---
+++ sequential_search/sequential_search/sequential_search.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int sequentialSearch(vector<int> arr, int element) {
+  if (arr.size() == 0) return -1;
+
+  for (int i = 0; i < arr.size(); i++) {
+    if (arr[i] == element) return i;
+  }
+  
+  return -1;
+}
+
+int quickSequentialSearch(vector<int> arr, int element) {
+  if (arr.size() == 0) return -1;
+
+  int lenght = arr.size();
+
+  int i = 0;
+  
+  while (arr[i] != element) {
+    i++;
+  }
+  if (i < lenght) {
+    return i;
+  }
+
+  return -1;
+}
+
+int main()
+{
+  vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8, };
+  cout << quickSequentialSearch(arr, 7) << endl;
+  cout << sequentialSearch(arr, 7) << endl;
+
+  return 0;
+}
