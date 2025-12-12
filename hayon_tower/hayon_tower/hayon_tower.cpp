ec86c3f6184cbcc58a064faa02b82fc81c4c77f4
Roman <topmanov@gmail.com>
2025-12-12 09:36:35 +0200

    hayon tower was added

---
+++ hayon_tower/hayon_tower/hayon_tower.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+void hayonTower(int n, char A, char B, char C) {
+	if (n == 0) return;
+
+	hayonTower(n - 1, A, C, B);
+
+	cout << "Move disk " << n << " from " << A << " to " << B << endl;
+
+	hayonTower(n - 1, C, B, A);
+}
+
+int main()
+{
+	hayonTower(3, 'A', 'B', 'C');
+	return 0;
+}
