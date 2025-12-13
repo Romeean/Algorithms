15c2d8c3e0f1092f9279d6aa284e902e2da2da90
Roman <topmanov@gmail.com>
2025-12-13 18:15:49 +0200

    heap sort update

---
+++ heap_sort/heap_sort/heap_sort.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+void showHeap(vector<int> arr) {
+  if (arr.size() == 0) return;
+  
+  int length = arr.size();
+  int level = 0;
+
+  for (int i = 0; i < length; i++) {
+    
+    int left = (1 << level) - 1;
+    int right = (1 << level + 1) - 2;
+
+    for (int j = left; j <= right && right < length; j++) {
+      cout << arr[j] << " ";
+    }
+    cout << endl;
+    level++;
+  }
+
+ }
+
+void heapify(const vector<int> &arr, int length, int i) {
+  
+  int largest = i;
+  int left = 2 * i + 1;
+  int right = 2 * i + 2;
+
+  if(left < length && arr[largest] < arr[left]){
+    largest = left;
+  }
+  if (right < length && arr[largest] < arr[right]) {
+    largest = right;
+  }
+  if (largest != i) {
+    swap(arr[largest], arr[i]);
+    heapify(arr, length, largest);
+  }
+
+}
+vector<int> heapSort(vector<int> arr) {
+  if (arr.size() == 0) return arr;
+  
+  int length = arr.size();
+  
+  for (int i = length / 2 - 1; i >= 0; i--) {
+    heapify(arr, length, i);
+  };
+
+  for (int i = length - 1; i >= 0; i--) {
+    swap(arr[i], arr[0]);
+    heapify(arr, length, 0);
+  }
+  
+  return arr;
+}
+
+int main()
+{
+  vector<int> arr = { 1, 2, 3, 4, 5, 6, 7 };
+  vector<int> result = heapSort(arr);
+  showHeap(result);
+  
+
+  return 0;
+}
