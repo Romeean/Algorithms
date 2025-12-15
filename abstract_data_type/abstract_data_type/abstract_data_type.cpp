6a50cb27894ed859f783e7550fce2422ae1adfb5
Roman <topmanov@gmail.com>
2025-12-15 10:37:26 +0200

    added methods: insert and locate

---
+++ abstract_data_type/abstract_data_type/abstract_data_type.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>
+
+using namespace std;
+//– INSERT(x, p, L).Вставити елемент x у позицію p у списку L.
+ 
+//– LOCATE(x, L).Повертає позицію елемента x у списку L.
+
+//– RETRIEVE(p, L).Повертає елемент, що стоїть в позиції p списку
+
+//– DELETE(p, L).Цей оператор видаляє елемент у позиції р списку
+
+//– NEXT(p, L), PREVIOUS(p, L).Ці функції повертають відповідно наступну й попередню позиції від позиції р у списку L.
+ 
+//– MAKENULL(L).Ця функція робить список L порожнім.
+
+//– FIRST(L).Ця функція повертає першу позицію в списку L.
+
+//– PRINTLIST(L).Друкує елементи списку L у порядку їх розташування.
+
+
+struct Node {
+	int value;
+	Node* nextNode;
+};
+
+struct List {
+	Node* head;
+};
+
+void insert(int x, int pisition, List &List){
+
+	int i = 0;
+	//while()
+}
+
+
+int main()
+{
+	
+	return 0;
+}
+
