519050a8ab28dd1d3b6fde395ec5593aa30c6c19
Roman <topmanov@gmail.com>
2025-12-18 10:14:10 +0200

    abstract data type update

--- abstract_data_type/abstract_data_type/abstract_data_type.cpp
+++ abstract_data_type/abstract_data_type/abstract_data_type.cpp
@@ -28,16 +28,146 @@ struct List {
 	Node* head;
 };
 
-void insert(int x, int pisition, List &List){
-
+void insert(int x, int position, List &List){
+	if (position == 0) {
+		Node* currentNode = List.head;
+		Node* newNode = new Node{ x, nullptr };
+		newNode->nextNode = currentNode;
+		List.head = newNode;
+		return;
+	}
+	
+	Node* currentNode = List.head;
 	int i = 0;
-	//while()
+	while (currentNode != nullptr) {
+		
+		if (position - 1 == i) {
+			Node* nodeNext = currentNode->nextNode;    // получаем ноду на элемент i + 1;
+			Node* newNode = new Node{ x, nullptr };    // создаем новый элемент
+
+			newNode->nextNode = currentNode->nextNode; // записываем для новой ноды ссылку на i + 1;
+			currentNode->nextNode = newNode; 
+			return;
+		}
+		currentNode = currentNode->nextNode;
+		i++;
+	}
+	return;
 }
 
+int locate(int x ,List &List) {
+	Node* currentNode= List.head;
+
+	int i = 0;
+	while(currentNode!= nullptr) {
+		
+		if (currentNode->value == x) {
+			return i;
+		}
+
+		currentNode= currentNode->nextNode;
+		i++;
+	}
+
+	return -1;
+}
+
+int retrive(int position, List& List) {
+	Node* currentNode= List.head;
+	int i = 0;
+	while(currentNode!= nullptr){
+		if (i == position) {
+			return currentNode->value;
+		}
+
+		currentNode= currentNode->nextNode;
+		i++;
+	}
+
+	return -1;
+}
+
+void remove(int position, List& List) {
+	if (position == 0) {
+		Node* currentNode = List.head;
+		currentNode->nextNode = nullptr;
+
+		delete currentNode;
+		
+	}
+	
+	Node* currentNode = List.head;
+
+	int i = 0;
+	while (currentNode != nullptr) {
+		if (position - 1 == i) {
+			Node* nodeToDelete = currentNode->nextNode;
+			Node* nextNode = nodeToDelete->nextNode;
+
+			currentNode->nextNode = nextNode;
+
+			delete nodeToDelete;
+
+		}
+		currentNode = currentNode->nextNode;
+	}
+}
+
+int next(int position, List& List) {
+	Node* currentNode = List.head;
+
+	int i = 0;
+	while (currentNode != nullptr) {
+		if (position + 1 == i) {
+			return currentNode->value;
+		}
+		currentNode = currentNode->nextNode;
+		i++;
+	}
+
+}
+
+int previous(int position, List &List) {
+	Node* currentNode = List.head;
+	
+	int i = 0;
+	while(currentNode != nullptr) {
+		if (position - 1 == i) {
+			return currentNode->value;
+		}
+		i++;
+	}
+
+}
+
+void makeNull(List &List) {
+	Node* currentNode = List.head;
+
+	int i = 0;
+	while (currentNode != nullptr) {
+		currentNode = nullptr;
+		currentNode = currentNode->nextNode;
+	}
+}
+
+int first(List &List) {
+	return List.head->value;
+}
+
+void showList(List &List) {
+
+	Node* currentNode = List.head;
+
+	while (currentNode != nullptr) {
+		cout << currentNode->value << " ";
+
+		currentNode = currentNode->nextNode;
+	}
+}
 
 int main()
 {
-	
+	List List;
 	return 0;
 }
 
