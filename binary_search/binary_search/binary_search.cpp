e804c0f18d679c455ec4ab4e13cf94a545312658
Roman <topmanov@gmail.com>
2025-12-20 17:44:58 +0200

    empirical analysi has been added to binary search

--- binary_search/binary_search/binary_search.cpp
+++ binary_search/binary_search/binary_search.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -29,10 +31,74 @@ int binarySearch(vector<int> arr, int element) {
   return -1;
 }
 
+vector<int> createSortedVector(int size) {
+  vector<int> arr(size);
+
+  for (int i = 0; i < size; i++) {
+    arr[i] = i;   
+  }
+
+  return arr;
+}
+
 
 int main()
 {
-  vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
-  cout << binarySearch(arr, 7);
+  srand(time(nullptr));
+
+  int choice = -1;
+
+  while (choice != 0){
+    cout << "\nMenu:\n";
+    cout << "1 - Binary search (single test)\n";
+    cout << "2 - Empirical analysis (binary search)\n";
+    cout << "0 - Exit\n";
+    cout << "Your choice: ";
+    cin >> choice;
+
+    if (choice == 1) {
+
+      vector<int> arr = { 1,2,3,4,5,6,7,8 };
+      int element;
+
+      cout << "Enter element to search: ";
+      cin >> element;
+
+      int index = binarySearch(arr, element);
+
+      if (index != -1)
+        cout << "Element found at index: " << index << endl;
+      else
+        cout << "Element not found\n";
+    }
+
+    if (choice == 2) {
+
+      int tests[3] = { 100000, 1000000, 10000000 };
+
+      cout << "\nEmpirical analysis of binary search\n";
+      cout << "----------------------------------\n";
+
+      for (int i = 0; i < 3; i++) {
+
+        int sizeOfArray = tests[i];
+        vector<int> arr = createSortedVector(sizeOfArray);
+
+        int target = rand() % sizeOfArray;
+
+        clock_t start = clock();
+        binarySearch(arr, target);
+        clock_t end = clock();
+
+        double timeSpent = double(end - start) / CLOCKS_PER_SEC;
+
+        cout << "n = " << sizeOfArray
+          << " | time = " << timeSpent
+          << " seconds" << endl;
+      }
+    }
+
+  } 
+
   return 0;
 }
